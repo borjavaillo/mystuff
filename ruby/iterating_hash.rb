869b2d4c65ef205506ede5ae2fbae34e896e08c4
Borja Vaillo <borjavaillo@hotmail.com>
2015-01-25 23:23:27 +0100

    iterating a hash

---
+++ ruby/iterating_hash.rb
@@ -0,0 +1,6 @@
+#!/usr/local/bin/ruby
+my_hash = {"First name"=>"Yukihiro", "Last name"=>"Matsumoto", "Age"=>47, "Nationality"=>"Japanese", "Nickname"=>"Matz"} 
+
+my_hash.each do |key, value|
+  puts key, my_hash[key]
+end
