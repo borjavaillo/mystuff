1a867409bd26d960e0bbac66d38203ded00b2891
Borja Vaillo <borja.vaillo@wuaki.tv>
2015-01-27 13:15:41 +0100

    new script

---
+++ aws/sg_on_running_instances_dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# This script lists all running instances SG and, for each one of them, outputs the list of SG that contain it ( dependency relationship )
+
+RUNNING_EC2_SG_LIST=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=running" --output=json | jq '.Reservations[].Instances[].SecurityGroups[].GroupName' | sed -e 's/"//g'` 
+
+for i in $RUNNING_EC2_SG_LIST
+  do
+    echo 
+    echo "SECURITY GROUP ${i} is contained by:"
+    aws ec2 describe-security-groups --output=json --filters "Name=group-name,Values=${i}" | jq '.SecurityGroups[].IpPermissions[].UserIdGroupPairs[].GroupName' | sort -u
+done
